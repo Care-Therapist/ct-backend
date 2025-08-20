85adf90984bea1eec612a6910ec4900067cd6a4a
2zm00 <dwa010301@gmail.com>
2025-08-20 18:03:24 +0900

    init: init project

---
+++ src/main/java/com/ct/ctbackend/CtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ct.ctbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ct/ctbackend/CtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.ctbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
